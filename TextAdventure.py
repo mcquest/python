66353aff1641b69626454ccb0dfb1d9c643f65ee
charlesfarris <80595817+charlesfarris@users.noreply.github.com>
2021-10-22 16:04:11 -0700

    Add files via upload

---
+++ TextAdventure.py
@@ -0,0 +1,44 @@
+"""
+Example Text Adventure..
+Script Name: TextAdventure.py
+Coder Name: Charles 
+What happens when the script is run?:
+User is given a series of choices and must make decisions
+to discover and adapt to their environment
+"""
+# Import time class to use functions in it 
+import time
+
+# Introductory decisions
+def Welcome():
+    # Opening Line
+    start = input("You are sitting outside on a bench and it starts to"
+                " rain, \n do you want to use your umbrella or go inside? (u/i) : ")
+    # Check user response to start with if statement
+    if start == 'i':
+        print("You get up and enter an abandoned super market...")
+        # add a delay
+        time.sleep(4)
+        # function call for stage 1 logic
+        market1()
+    #  Opening umbrella user response
+    elif start == 'u':
+        print("The umbrella opens...")
+        time.sleep(8)
+    # Incorrect input
+    else:
+        # rerun the function
+        # (recursive statement
+        #           - this type of function call typically has a different input than the original)
+        Welcome()
+
+# Stage 1 
+def market1():
+        # user response is inside1
+        inside1 = input("You see a dim light in the back left corner \n
+                        "and hear a muffled buzz coming from"
+                      "the back right. Investigate (L/R): ")
+
+Welcome()
+
+
---
+++ YourNameFUN.py
@@ -0,0 +1,29 @@
+"""
+YourNameFUN.py
+Lab 1 - First Functions Script
+Requirements:
+Every line of code should be commented.
+3 functions
+"""
+#1) Prints a greeting to the console
+def HelloWorld():
+    
+#2) Has two or more inputs and prints their value when added together
+def Adder(i, j):
+
+#3) - Asks the user for input,
+#     - Uses an if statement to check the input value,
+#    - Returns a string to the console based on the user input
+def WhatIsTheBestGame():
+    # A statement to take user input
+    userInput = input("What do you want to ask the user?: ")
+    # Now check answer7 with an if statement
+    if userInput == "correct answer":
+        # Do something
+    else:
+        # other thing
+
+WhatIsTheBestGame()
+
+Adder(4,4)
+
