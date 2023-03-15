6231f5cca35439052d996255107768c2f768e66a
mcquest <80595817+mcquest@users.noreply.github.com>
2023-03-15 15:01:22 -0700

    Add files via upload

---
+++ Num_System_Translations.py
@@ -0,0 +1,49 @@
+#mcquest
+#input one number system to output another
+def Base2to10():
+    b =  input("Enter a binary number: ")
+    j = 0
+    t = 0
+    l = len(b)-1
+    while j <= l:
+        if b[l-j] == "1":
+            t = t + int(b[l-j]) * (2 ** j)
+        j +=1
+        
+    print(t)
+
+#Base2to10()
+
+def Base10to2Builtin():
+    i = input("Enter an integer: ")
+    print(bin(int(i)))
+
+def Base10to2A():
+    ii = input("Enter an integer: ")
+    i = int(ii)
+    exp = 0
+    num0 = []
+    expCheck = 0
+    while i > 2**exp:
+         exp +=1
+    lexp = exp - 1
+    num0 = ["0"]*exp
+    #num0[expCheck] = "1"
+    while expCheck < exp:
+        if i >= 2**lexp:
+            num0[expCheck] = "1"
+            expCheck+=1
+            i = i - (2**lexp)
+            lexp -= 1
+        else:
+            expCheck+=1
+            lexp -= 1
+    print(''.join(num0))
+Base10to2A()
+
+"""
+With mod % and division
+    if i % 2 == 0:
+        num0 = "0" + num0
+        i = i / 2
+"""
