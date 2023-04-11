47d4e958d8fa4bd52ff394a13953bd392cec5aea
mcquest <80595817+mcquest@users.noreply.github.com>
2023-04-11 11:25:16 -0700

    Create TextAdventureStructure.py

---
+++ TextAdventureStructure.py
@@ -0,0 +1,22 @@
+# Comments
+
+def fname2():
+    print("Story here")
+     v2 = input("Choose d, j, or k ? [Then press enter] ")
+    if v2 == "k":
+        fName4()
+
+   elif v2 == "j":
+        fName5()
+
+#function
+def fName1():
+    print("Story here")
+    v1 = input("Choose a, b, or c? [Then press enter] ")
+    if v1 == "a":
+        fName2()
+
+    elif v1 == "b":
+        fName3()
+
+fname1()
