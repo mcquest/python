53fc02bfce2feb71783c8b2a788102ce94c48633
mcquest <80595817+mcquest@users.noreply.github.com>
2023-04-28 09:34:46 -0700

    Update Reference.py

--- Reference.py
+++ Reference.py
@@ -100,6 +100,45 @@ def print_scroll(output):
         sys.stdout.flush()
         sleep(.02) #seconds
 
+# Inspired by J C        
+# Define a function to convert a number from one base to another
+def base_conversion(number, from_base, to_base):
+    # First, convert the number to base 10
+    base_10_num = 0
+    for i in range(len(number)):
+        digit = int(number[i], from_base)
+        power = len(number) - 1 - i
+        base_10_num += digit * (from_base ** power)
+    
+    # Next, convert the base 10 number to the target base
+    result = ""
+    while base_10_num > 0:
+        digit = base_10_num % to_base
+        result = str(digit) + result
+        base_10_num //= to_base
+    
+    return result
+
+# Define a function to prompt the user for input and output bases and perform the conversion
+def translate_number():
+    # Prompt the user for the input number and its base
+    number = input("Enter the number to convert: ")
+    from_base = int(input("Enter the base of the input number: "))
+    
+    # Prompt the user for the output base
+    to_base = int(input("Enter the base of the output number: "))
+    
+    # Convert the number to the output base
+    result = base_conversion(number, from_base, to_base)
+    
+    # Print the result
+    print("The converted number is:", result)
+
+# Call the translate_number() function to start the program
+translate_number()
+
+
+
 """
 TODO
 Truth tables
